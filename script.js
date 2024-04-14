304807c554e3bcad5d77a9effae246b56430b755
Kristina Mae <kristinamae.medrano@gmail.com>
2024-04-14 19:21:29 +0800

    1st

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+var typingEffect = new Typed(".typedText",{
+  strings : ["Sentinel"],
+  loop : true,
+  typeSpeed : 100, 
+  backSpeed : 80,
+  backDelay : 2000
+})
